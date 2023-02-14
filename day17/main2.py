409d5446be7f6cd68a93fe5569b757d1d7931172
tehdi <tehdi@users.noreply.github.com>
2023-02-14 16:28:59 -0400

    Day 17: Part 2: YES! I had some initial state stuff wrong and it was breaking the entire thing, as incorrect initial state tends to do. But it's fixed! And I found the repeating cycle I needed! And I did math and I got it RIGHT!

--- day17/main2.py
+++ day17/main2.py
@@ -19,7 +19,8 @@ def configure_logging(verbose, output_file):
         )
 
 class ShapeNode:
-    def __init__(self, coordinates):
+    def __init__(self, id, coordinates):
+        self.id = id
         self.start_position = coordinates
         self.coordinates = coordinates
         self.next = None
@@ -53,6 +54,30 @@ class DirectionNode:
     def has_next(self):
         return self.next is not None
 
+class State:
+    def __init__(self, cave_profile, shape_id, direction_index):
+        self.cave_profile = tuple(cave_profile)
+        self.shape_id = shape_id
+        self.direction_index = direction_index
+
+    def __hash__(self):
+        return hash((
+            self.shape_id,
+            self.direction_index,
+            self.cave_profile
+        ))
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)): return NotImplemented
+        return (
+            self.shape_id == other.shape_id
+                and self.direction_index == other.direction_index
+                and self.cave_profile == other.cave_profile
+        )
+
+    def __str__(self):
+        return f"Cave profile: {self.cave_profile}, Shape: {self.shape_id}, Direction: {self.direction_index}"
+
 def has_landed(coordinates, cave):
     for (x, y) in coordinates:
         if y == 0: return True
@@ -63,22 +88,52 @@ def land(shape, cave):
     for (x, y) in shape.coordinates:
         cave[y].add(x)
 
+def is_repeated_state(normalized_profile, shape_id, direction_index, states, height, rock_count):
+    latest_state = State(normalized_profile, shape_id, direction_index)
+    if latest_state in states:
+        (matched_rock_count, matched_height) = states[latest_state]
+        delta_rock_count = rock_count - matched_rock_count
+        delta_height = height - matched_height
+        logging.debug(f"Found a repeated state at rock {rock_count}: {str(latest_state)}")
+        logging.debug(f"Rock count: {matched_rock_count} -> {rock_count} = {delta_rock_count}")
+        logging.debug(f"Height: {matched_height} -> {height} = {delta_height}")
+        return (True, delta_rock_count, delta_height)
+    else:
+        states[latest_state] = (rock_count, height)
+        return (False, 0, 0)
+
+def update_profile(shape_node, cave_profile):
+    for x,y in shape_node.coordinates:
+        if y > cave_profile[x]: cave_profile[x] = y
+    baseline = min(cave_profile)
+    return [(y-baseline) for y in cave_profile]
+
 def drop_rocks(total_rocks, shape_head, direction_head, cave):
     shape_node = shape_head
     direction_node = direction_head
     rocks_landed = 0
     highest_y = 0
     jet = True
+    cave_profile = [0]*7 # max y value for each x==index
+    states = {}
+    # draw_cave(shape_node, cave, 10)
     while rocks_landed < total_rocks:
         if not jet and has_landed(shape_node.coordinates, cave):
             land(shape_node, cave)
             highest_y = max(highest_y, shape_node.highest_y())
+            normalized_profile = update_profile(shape_node, cave_profile)
             rocks_landed += 1
             if rocks_landed % LOG_EVERY == 0:
                 logging.info(f"Tower height after {rocks_landed} rocks have landed: {highest_y + 1}")
+                logging.info(f"Tower height after {rocks_landed} rocks have landed: {highest_y + 1}")
+                logging.debug(f"Cave profile: {normalized_profile}; Shape: {shape_node.id}; Direction: {direction_node.index}")
+            (repeated, rock_delta, y_delta) = is_repeated_state(normalized_profile, shape_node.id, direction_node.index, states, highest_y+1, rocks_landed)
+            # logging.debug("Landed")
+            # draw_cave(shape_node, cave, highest_y + 1)
             shape_node = shape_node.next
             shape_node.reset_position()
-            shape_node.try_move(start_x_offset, highest_y + start_y_offset, min_x, max_x, cave)
+            shape_node.try_move(start_x_offset, highest_y + start_y_offset + 1, min_x, max_x, cave)
+            # draw_cave(shape_node, cave, highest_y + start_y_offset + 5)
             jet = True
             continue
         x, y = 0, 0
@@ -91,27 +146,40 @@ def drop_rocks(total_rocks, shape_head, direction_head, cave):
             y = -1
         shape_node.try_move(x, y, min_x, max_x, cave)
         jet = not jet
-    return highest_y
+        # draw_cave(shape_node, cave, highest_y + start_y_offset + 5)
+    return (rocks_landed, highest_y)
+
+def draw_cave(shape, cave, max_y):
+    logging.debug(shape.coordinates)
+    logging.debug('~~~~~~~')
+    for y in range(max_y, max(-1, max_y-25), -1):
+        line = ''
+        for x in range(7):
+            line += '#' if ((x, y) in shape.coordinates or x in cave[y]) else '.'
+        logging.debug(line)
+    logging.debug('~~~~~~~')
+    logging.debug('')
+    # input()
 
 # x increases to the right, y increases up
 #  ####
-SHAPE_1 = ShapeNode([(0, 0), (1, 0), (2, 0), (3, 0)])
+SHAPE_1 = ShapeNode(1, [(0, 0), (1, 0), (2, 0), (3, 0)])
 #   #
 #  ###
 #   #
-SHAPE_2 = ShapeNode([(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)])
+SHAPE_2 = ShapeNode(2, [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)])
 #    #
 #    #
 #  ###
-SHAPE_3 = ShapeNode([(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)])
+SHAPE_3 = ShapeNode(3, [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)])
 #  #
 #  #
 #  #
 #  #
-SHAPE_4 = ShapeNode([(0, 0), (0, 1), (0, 2), (0, 3)])
+SHAPE_4 = ShapeNode(4, [(0, 0), (0, 1), (0, 2), (0, 3)])
 #  ##
 #  ##
-SHAPE_5 = ShapeNode([(0, 0), (1, 0), (0, 1), (1, 1)])
+SHAPE_5 = ShapeNode(5, [(0, 0), (1, 0), (0, 1), (1, 1)])
 
 DIRECTION = { '>': 1, '<': -1 }
 
@@ -157,11 +225,11 @@ if __name__ == '__main__':
     # new rocks spawn with 2 empty spaces on their left,
     # and 3 empty spaces below them (regardless of current built-up rock height)
     start_x_offset = 2
-    start_y_offset = 4
+    start_y_offset = 3
 
     rocks = args.rocks
     cave = defaultdict(set)  # y: set(x, x, x, ...)
     shape_head = SHAPE_1
     shape_head.try_move(start_x_offset, start_y_offset, min_x, max_x, cave)
-    highest_y = drop_rocks(rocks, shape_head, direction_head, cave)
-    logging.info(f"Tower height after {rocks} rocks have landed: {highest_y + 1}")
+    rocks_landed, highest_y = drop_rocks(rocks, shape_head, direction_head, cave)
+    logging.info(f"Tower height after {rocks_landed} rocks have landed: {highest_y + 1}")
